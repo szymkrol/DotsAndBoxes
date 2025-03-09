f73b218e449d370aa578740c3be25aa6f3362f7c
szymkrol <szymon.krol0107@gmail.com>
2025-03-09 21:52:55 +0100

    Partitioned minmax function

--- minmax.py
+++ minmax.py
@@ -5,42 +5,29 @@ def evaluate_move(game, is_maximising, depth, alpha, beta, use_alpha_beta, move_
         return minmax(game, depth - 1, not is_maximising, alpha, beta, use_alpha_beta)
 
 
+def perform_iteration(game, depth, is_maximising, alpha=-1000, beta=1000, use_alpha_beta=True):
+    best_value = -1000 if is_maximising else 1000
+    available_moves = game.get_available_moves().copy()
+    best_move = available_moves[0]
+    for move in available_moves:
+        move_result = game.edge_play(move, is_maximising)
+        evaluated_value, evaluated_move = evaluate_move(game, is_maximising, depth, alpha, beta, use_alpha_beta,
+                                                        move_result)
+        if (is_maximising and evaluated_value > best_value) or (not is_maximising and evaluated_value < best_value):
+            best_move = move
+            best_value = evaluated_value
+        if use_alpha_beta:
+            alpha, beta = (max(alpha, best_value), beta) if is_maximising else (alpha, min(beta, best_value))
+            if beta <= alpha:
+                game.edge_unplay(move, is_maximising)
+                break
+        game.edge_unplay(move, is_maximising)
+    return best_value, best_move
+
+
 def minmax(game, depth, is_maximising, alpha=-1000, beta=1000, use_alpha_beta=True):
     available_moves = game.get_available_moves().copy()
     # End node
     if depth == 0 or len(available_moves) == 0:
         return game.get_score(), None
-    if is_maximising:
-        best_value = -1000
-        best_move = available_moves[0]
-        for move in available_moves:
-            move_result = game.edge_play(move, is_maximising)
-            evaluated_value, evaluated_move = evaluate_move(game, is_maximising, depth, alpha, beta, use_alpha_beta, move_result)
-            if evaluated_value > best_value:
-                best_move = move
-                best_value = evaluated_value
-            # Alpha/beta
-            if use_alpha_beta:
-                alpha = max(alpha, best_value)
-                if beta <= alpha:
-                    game.edge_unplay(move, is_maximising)
-                    break
-            game.edge_unplay(move, is_maximising)
-        return best_value, best_move
-    else:  # Minimising player
-        best_value = 1000
-        best_move = available_moves[0]
-        for move in available_moves:
-            move_result = game.edge_play(move, is_maximising)
-            evaluated_value, evaluated_move = evaluate_move(game, is_maximising, depth, alpha, beta, use_alpha_beta, move_result)
-            if evaluated_value < best_value:
-                best_move = move
-                best_value = evaluated_value
-            # Alpha/beta
-            if use_alpha_beta:
-                beta = min(beta, best_value)
-                if beta <= alpha:
-                    game.edge_unplay(move, is_maximising)
-                    break
-            game.edge_unplay(move, is_maximising)
-        return best_value, best_move
+    return perform_iteration(game, depth, is_maximising, alpha, beta, use_alpha_beta)
